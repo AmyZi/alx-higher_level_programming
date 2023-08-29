8cf79cc6fe9502c87074566c118b6bc20336df57
amyzi <ebiojoidris@icloud.com>
2023-08-29 09:15:27 -0700

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,28 +3,27 @@
 
 
 class Square:
-    """square Class
+    """Square Class
     Attributes:
-        __size (int): size of a side of the sqr
+        __size (int): size of a side of the Square
     """
     def __init__(self, size=0):
-        """ initializes the sqr
+        """ initializes the Square
         Args:
-            size (int): size of a side of the sqr
+            size (int): size of a side of the Square
         Returns:
             None
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
     def area(self):
-        """calculates the sqr's area
+        """calculates the Square's area
         returns:
-            The area of the sqr
+            The area of the Square
         """
         return (self.__size) ** 2
