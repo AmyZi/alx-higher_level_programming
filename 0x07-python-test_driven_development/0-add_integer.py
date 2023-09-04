e87de05055b9079d7bf20ce9fd820f5f93fe423b
amyzi <ebiojoidris@icloud.com>
2023-09-04 10:58:41 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 """
-This is the "0-add_integer" module.
+This is the "add_integer" module.
 The 0-add_integer module supplies one function, add_integer(a, b).
 """
 
 
-def add_integer(a, b):
-    """ Return the addition of two numbers."""
+def add_integer(a, b=98):
+    """Return the addition of two numbers"""
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be aninteger")
+        raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
-        raise TypeError("a must be aninteger")
+        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
     return a + b
+
+if __name__ == "__main__":
+    # Specify the path to the test file in the 'test' directory.
+    test_file = 'test/0-add_integer.txt'
+
+    # Run the doctests from the specified file.
+    doctest.testfile(test_file, verbose=True)
